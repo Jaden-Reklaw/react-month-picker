f9b6550918842c07c8aad13f0d7e237931db669c
Jordan Walker <jwalker117@gmail.com>
2020-04-06 10:12:57 -0500

    Use axios to retrieve (GET) data from /calendar and store it in App.js. /calendar returns an array of calendar month names and ids.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,6 +3,26 @@ import axios from 'axios';
 import './App.css';
 
 class App extends Component {
+  state = {
+    monthList: [],
+  }
+
+  //Run this as soon as App loads
+  componentDidMount() {
+    this.getMonths();
+  }
+
+  //Method used to do an ajax call to the server to request months from the calender.data.js
+  getMonths = () => {
+    axios.get('/calendar').then( (response) => {
+        console.log(response.data);
+        this.setState({monthList: response.data});
+      }).catch( (error) => {
+        alert('Bad things happened...');
+        console.log('Error on GET calendar', error);
+      });
+  }
+
   render() {
     return (
       <div className="App">
