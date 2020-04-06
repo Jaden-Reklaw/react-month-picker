4f245d1efe6ef3b2a7263567822f9d9667d24acf
Jordan Walker <jwalker117@gmail.com>
2020-04-06 10:17:07 -0500

    Added Header Component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import './App.css';
 
+//Import Components
+import Header from '../Header/Header'
+
 class App extends Component {
   state = {
     monthList: [],
@@ -26,11 +29,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Select a Month</h1>
-          <h3>SELECTED MONTH GOES HERE</h3>
-          <br/>
-        </header>
+        <Header />
         <br/>
         <p>List of months goes here</p>
       </div>
---
+++ src/components/Header/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './Header.css';
+
+
+const Header = () => {
+    return (
+        <header>
+          <h1>Select a Month</h1>
+          <h3>SELECTED MONTH GOES HERE</h3>
+        </header>
+    );
+}
+
+export default Header;
